0455c033ffd782c8696b32214e1a19751d276af5
Matheus Henrique de Oliveria <mthws.henrique@hotmail.com>
2022-06-29 15:34:53 -0300

    Update calculadora.dart

--- lib/view/calculadora.dart
+++ lib/view/calculadora.dart
@@ -2,19 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Calculadora extends StatelessWidget {
-  int? numero0;
-  int? numero1;
-  int? numero2;
-  int? numero3;
-  int? numero4;
-  int? numero5;
-  int? numero6;
-  int? numero7;
-  int? numero8;
-  int? numero9;
-  String? operacaoSoma;
-  String? operacaoSubtracao;
-
+  String operacao = '+';
+  final memoria = [0.0, 0.0];
+  String resultado = '';
+  Widget botao(String nomeBotao, VoidCallback funcao) {
+    return ElevatedButton(
+      child: Text(nomeBotao),
+      onPressed: funcao,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,99 +20,127 @@ class Calculadora extends StatelessWidget {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
-        
         children: [
           Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  child: Text("7"),
-                  onPressed: () {
-                    numero7 = 7;
-                  }),
-              ElevatedButton(
-                  child: Text("8"),
-                  onPressed: () {
-                    numero8 = 8;
-                  }),
-              ElevatedButton(
-                  child: Text("9"),
-                  onPressed: () {
-                    numero9 = 9;
-                  }),
-            ],
-          ),
-          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                  child: Text("4"),
-                  onPressed: () {
-                    numero4 = 4;
-                  }),
-              ElevatedButton(
-                  child: Text("5"),
-                  onPressed: () {
-                    numero5 = 5;
-                  }),
-              ElevatedButton(
-                  child: Text("6"),
-                  onPressed: () {
-                    numero6 = 6;
-                  }),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  child: Text("1"),
-                  onPressed: () {
-                    numero1 = 1;
-                  }),
-              ElevatedButton(
-                  child: Text("2"),
-                  onPressed: () {
-                    numero2 = 2;
-                  }),
-              ElevatedButton(
-                  child: Text("3"),
-                  onPressed: () {
-                    numero3 = 3;
-                  }),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  child: Text("+"),
-                  onPressed: () {
-                    operacaoSoma = "+";
-                  }),
-              ElevatedButton(
-                  child: Text("0"),
-                  onPressed: () {
-                    numero0 = 0;
-                  }),
-              ElevatedButton(
-                  child: Text("-"),
-                  onPressed: () { 
-                    operacaoSubtracao = "-";
-                  }),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(176,40),
-                ),
-                child:Text("=",),
-                onPressed: (){
-
+              botao("AC", () {
+                operacao = '';
+                memoria[0] = double.parse(resultado);
+                resultado = '';
               }),
+              botao("CE", () {
+                operacao = 'CE';
+                memoria[0] = double.parse(resultado);
+                resultado = '';
+              }),
+              botao("%", () {
+                operacao = '%';
+                memoria[0] = double.parse(resultado);
+                resultado = '';
+              }),
+              botao("/", () {
+                operacao = '/';
+                memoria[0] = double.parse(resultado);
+                resultado = '';
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              botao("7", () {
+                resultado += "7";
+              }),
+              botao("8", () {
+                resultado += "8";
+              }),
+              botao("9", () {
+                resultado += "9";
+              }),
+              botao("x", () {
+                operacao = 'x';
+                memoria[0] = double.parse(resultado);
+                resultado = '';
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              botao("4", () {
+                resultado += "4";
+              }),
+              botao("5", () {
+                resultado += "5";
+              }),
+              botao("6", () {
+                resultado += "6";
+              }),
+              botao("-", () {
+                operacao = '-';
+                memoria[0] = double.parse(resultado);
+                resultado = '';
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              botao("1", () {
+                resultado += "1";
+              }),
+              botao("2", () {
+                resultado += "2";
+              }),
+              botao("3", () {
+                resultado += "3";
+              }),
+              botao("+", () {
+                operacao = '+';
+                memoria[0] = double.parse(resultado);
+                resultado = '';
+              }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              botao("0", () {
+                resultado += "0";
+              }),
+              botao("=", () {
+                if (resultado == '') {
+                  print("0");
+                } else {
+                  memoria[1] = double.parse(resultado);
+                  double calculo = 0.0;
+                  switch (operacao) {
+                    case "AC":
+                      calculo = 0.0;
+                      break;
+                    case "CE":
+                      calculo = memoria[0];
+                      break;
+                    case "%":
+                      calculo = memoria[0] % memoria[1];
+                      break;
+                    case "/":
+                      calculo = memoria[0] / memoria[1];
+                      break;
+                    case "x":
+                      calculo = memoria[0] * memoria[1];
+                      break;
+                    case "-":
+                      calculo = memoria[0] - memoria[1];
+                      break;
+                    case "+":
+                      calculo = memoria[0] + memoria[1];
+                      break;
+                  }
+                  print(calculo);
+                }
+              })
             ],
           ),
         ],
